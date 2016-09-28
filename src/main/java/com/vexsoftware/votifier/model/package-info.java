444fa736a4e72015d9a4dcae3d6fd97ff7c9b5ff
bebe0601 <jrhigasinihonn.0601@gmail.com>
2016-09-28 22:30:43 +0900

    package-info

---
+++ src/main/java/com/vexsoftware/votifier/model/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * Lisence and copyright
+ * https://raw.githubusercontent.com/vexsoftware/votifier/master/LICENSE
+ */
+/**
+ * This package is from the Votifier.
+ * Change does not conduct.
+ * @version 2.0
+ */
+package com.vexsoftware.votifier.model;
